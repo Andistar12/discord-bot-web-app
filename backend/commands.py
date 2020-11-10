0043ff19c3be0ad3de51ca9041a9b9d7f0097c2c
eugenetan3 <etanshs0@gmail.com>
2020-11-09 21:05:53 -0800

    Preliminary hash table and basic lookup execution from text_cmd

--- backend/commands.py
+++ backend/commands.py
@@ -3,28 +3,40 @@ import sys
 import math
 import text_cmd
 
+
+
 class BOT:
     def __init__(self):
         self.text_cmd = text_cmd.TEXT()
         self.start_time = time.time()
-        self.command_list = ["ping", "pong", "repeat"]
+        self.LOOK_UP = {"ping": self.text_cmd.ping, "pong": self.text_cmd.pong, "repeat": self.text_cmd.repeat}
 
     def getUptime(self):
         return time.time() - self.start_time
 
 class COMMAND(BOT):
-    def __init__(self, cmd):
+    def __init__(self, cmd, arguments):
         super().__init__()
         self.command = cmd
+        self.arguments = arguments
 
     def lookup(self):
-        if self.command in self.command_list:
+        if self.LOOK_UP[self.command]:
             return True
         else:
             return False
 
-    def pong(self):
-        return self.text_cmd.pong()
+    def execute(self):
+        if self.lookup():
+            return self.LOOK_UP[self.command](self.arguments)
+        else:
+            return "", 204
+
+    # def pong(self):
+    #     return self.LOOK_UP["pong"](self.arguments)
+    
+    # def repeat(self):
+    #     return self.LOOK_UP["repeat"](self.arguments)
 
     
 
