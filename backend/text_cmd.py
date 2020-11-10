9540f0eb1d9208ca35268b5cace27380b7b89a3c
eugenetan3 <etanshs0@gmail.com>
2020-11-09 21:06:08 -0800

    Function implementations (ping, pong, repeat)

--- backend/text_cmd.py
+++ backend/text_cmd.py
@@ -1,8 +1,25 @@
+import flask
+from flask import Flask, request, jsonify, Response, abort
 import commands
+import json
+import sys
+import os
 
 class TEXT:
     def __init__(self):
+        super().__init__()
         pass
 
-    def pong(self):
-        return {"response":"pong"}
+    def pong(self, arguments):
+        return {"response":"I say Ping!"}
+    
+    def ping(self, arguments):
+        return {"response":"I say Pong!"}
+    
+    def repeat(self, arguments):
+        response = {
+            "response": "Specify something for me to repeat!"
+        }
+        if len(arguments) > 0:
+            response["response"] = " ".join(arguments)
+        return jsonify(response)
