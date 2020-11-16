e6e99b12cda347e838f9b06cd1ed4a3aab47080e
eugenetan3 <etanshs0@gmail.com>
2020-11-16 11:25:00 -0800

    Updated table lookup

--- backend/commands.py
+++ backend/commands.py
@@ -1,18 +1,17 @@
 import time
 import sys
 import math
+import datetime
 import text_cmd
-
 
 
 class BOT:
     def __init__(self):
         self.text_cmd = text_cmd.TEXT()
-        self.start_time = time.time()
-        self.LOOK_UP = {"ping": self.text_cmd.ping, "pong": self.text_cmd.pong, "repeat": self.text_cmd.repeat, "eightball": self.text_cmd.eight_ball}
+        self.start_time = datetime.datetime.now()
+        self.LOOK_UP = {"ping": self.text_cmd.ping, "pong": self.text_cmd.pong, "repeat": self.text_cmd.repeat, "eightball": self.text_cmd.eight_ball, "uptime": self.text_cmd.uptime}
 
-    def getUptime(self):
-        return time.time() - self.start_time
+
 
 class COMMAND(BOT):
     def __init__(self, cmd, arguments):
@@ -31,7 +30,5 @@ class COMMAND(BOT):
             return self.LOOK_UP[self.command](self.arguments)
         else:
             return "", 204
-
-
     
 
