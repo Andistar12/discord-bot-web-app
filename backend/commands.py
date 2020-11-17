937b783c6ce59541f06da4d1121fe516b2ccdfba
eugenetan3 <etanshs0@gmail.com>
2020-11-16 19:55:51 -0800

    Added more commands, support for tagging and channels

--- backend/commands.py
+++ backend/commands.py
@@ -9,15 +9,22 @@ class BOT:
     def __init__(self):
         self.text_cmd = text_cmd.TEXT()
         self.start_time = datetime.datetime.now()
-        self.LOOK_UP = {"ping": self.text_cmd.ping, "pong": self.text_cmd.pong, "repeat": self.text_cmd.repeat, "eightball": self.text_cmd.eight_ball, "uptime": self.text_cmd.uptime}
+        self.LOOK_UP = {"ping": self.text_cmd.ping, "pong": self.text_cmd.pong, "repeat": self.text_cmd.repeat, "eightball": self.text_cmd.eight_ball, 
+                        "uptime": self.text_cmd.uptime, "choice": self.text_cmd.choice, "coinflip": self.text_cmd.coinflip}
 
 
 
 class COMMAND(BOT):
-    def __init__(self, cmd, arguments):
+    def __init__(self, cmd, arguments, userid, msgid, msgchid, priv):
         super().__init__()
         self.command = cmd
         self.arguments = arguments
+        self.user_id = userid
+        self.message_id = msgid
+        self.message_ch_id = msgchid
+        self.is_priv = priv
+        self.pack = {"command": self.command, "arguments": self.arguments, "user_id": self.user_id, "message_id": self.message_id, "message_channel_id": self.message_ch_id,
+                "is_private": self.is_priv}
 
     def lookup(self):
         if self.LOOK_UP[self.command]:
@@ -27,7 +34,7 @@ class COMMAND(BOT):
 
     def execute(self):
         if self.lookup():
-            return self.LOOK_UP[self.command](self.arguments)
+            return self.LOOK_UP[self.command](self.arguments, self.pack)
         else:
             return "", 204
     
